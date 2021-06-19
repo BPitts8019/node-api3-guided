6fc2e9fe03aaebf311eb548a7ae4a68d1c48a3d4
BPitts8019 <bradleyj.pitts@gmail.com>
2021-06-19 14:35:38 -0700

    Add Require User Agent custom middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const logger = require("./middleware/logger");
+const requireAgent = require("./middleware/agent");
 // const morgan = require("morgan");
 const PORT = 8080;
 const hubRouter = require("./routers/hub");
@@ -10,6 +11,7 @@ const server = express();
 
 server.use(helmet());
 server.use(logger());
+server.use(requireAgent("insomnia"));
 // server.use(morgan("short"));
 server.use(express.json());
 // Bring all our subroutes into the main application
